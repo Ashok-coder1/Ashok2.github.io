bd231eb8f8d8552c2d8f7fe8b08cecef9be375b0
Ashok-coder1 <homekhabar@gmail.com>
2026-03-19 12:51:22 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = {
+        fullName: form.fullName.value,
+        mobile: form.mobile.value,
+        email: form.email.value,
+        subject: form.subject.value,
+        message: form.message.value
+    };
+
+    try {
+        const res = await fetch('/submit', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData)
+        });
+
+        if (res.ok) {
+            window.location.href = "/thankyou.html";
+        } else {
+            alert("Something went wrong");
+        }
+
+    } catch (err) {
+        console.error(err);
+        alert("Server error");
+    }
+});
