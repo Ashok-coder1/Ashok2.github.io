c2f9583808bc43bfe67673a77d8cc01fe825c633
Ashok-coder1 <homekhabar@gmail.com>
2026-03-19 14:42:37 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const bodyParser = require('body-parser');  // <-- Add this line
 const Form = require('./models/Form');
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());                 // <-- now works
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // MongoDB Connect
